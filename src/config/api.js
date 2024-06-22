dc79577f9316a68a7b902ddd5607c412d6cc2624
LAVKESH PRASAD <lavkeshprasad@gmail.com>
2024-06-22 12:25:01 +0530

    first commit

--- src/config/api.js
+++ src/config/api.js
@@ -1,6 +1,6 @@
 const API_DOMAIN = "https://gnews.io/api/v4/top-headlines?country=";
 const API_SEARCH_DOMAIN = "https://gnews.io/api/v4/search?q=";
-const API_KEY = "3f4398cadb205a9d36aa0299955bf631";
+const API_KEY = "c9aed724fc14c78385cce498dd866006";
 export const endpointPath = (country, category) =>
   `${API_DOMAIN}${country}&lang=en&category=${category}&apikey=${API_KEY}`;
 export const endpointSearch = (searchQuery) =>
